f8877fb07f2dec42f61b5c07354031353d91b70d
gras2015 <gras.job76@gmail.com>
2023-06-27 15:51:45 +0200

    Add project files.

---
+++ ExcelControl/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace ExcelControl
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        private Microsoft.Office.Interop.Excel.Application excel;
+        private Workbook classeur;
+        private Worksheet feuille;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Le bouton qui lance Excel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.Visible = true;
+        }
+
+        /// <summary>
+        /// Ferme Excel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (excel != null)
+            {
+                excel.Quit();
+            }
+        }
+
+        /// <summary>
+        /// Méthode de traitement
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (excel != null)
+            {
+                classeur = excel.Workbooks.Add();
+                feuille = classeur.ActiveSheet;
+                feuille.Cells[2, 2] = "Hello world!";
+            }
+        }
+
+        /// <summary>
+        /// Bouton Couleur
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (feuille != null)
+            {
+               
+                feuille.Cells[2, 2].Interior.Color = XlRgbColor.rgbDarkGreen;
+
+            }
+                
+        }
+
+        /// <summary>
+        /// Créer une table de multiplication
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Button_Click(sender, e);
+            Button_Click_2(sender, e);
+            int max = 3;
+            if (feuille != null) 
+            {
+                for (int l = 1; l <= max; l++)
+                {
+
+                    for (int c = 1; c <= max; c++)
+                    {
+                        feuille.Cells[l, c] = l * c;
+                       
+                    }
+                }
+                Range lignes = feuille.Range[feuille.Cells[1, 1], feuille.Cells[max, max]];
+                lignes.Borders[XlBordersIndex.xlEdgeTop].LineStyle = XlLineStyle.xlDouble;
+                lignes.Borders[XlBordersIndex.xlEdgeBottom].LineStyle = XlLineStyle.xlDouble;
+                lignes.Borders[XlBordersIndex.xlEdgeRight].LineStyle = XlLineStyle.xlDouble;
+                lignes.Borders[XlBordersIndex.xlEdgeLeft].LineStyle = XlLineStyle.xlDouble;
+                lignes.Borders[XlBordersIndex.xlInsideHorizontal].LineStyle = XlLineStyle.xlDouble;
+            }
+
+        }
+        /// <summary>
+        /// Ferme l'application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ SpamLauncher/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Office.Interop.Outlook;
+
+
+namespace SpamLauncher
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Microsoft.Office.Interop.Outlook.Application outlook;
+        private MailItem mail;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            outlook = new Microsoft.Office.Interop.Outlook.Application();
+        }
+
+        /// <summary>
+        /// Générer des e-mails
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Créer notre objet mail
+
+            mail = outlook.CreateItem(OlItemType.olMailItem);
+            //On définit le sujet du mail
+
+            mail.Subject = "Ceci est un mail de test";
+            //On définit le ou les destinataires
+
+            mail.To = TextAdresse.Text; //Récupere la valeur du textbox
+            //On définit le corps du message
+            mail.Body = TextMessage.Text;
+            mail.Display(true);
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (mail != null)
+                if (!mail.Sent)
+                   mail.Send();
+                
+                    
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            int nBmail = 10;
+            for (int i =0; i<nBmail; i++)
+
+            {
+                mail = outlook.CreateItem(OlItemType.olMailItem);
+               
+
+                mail.Subject = "Mail n" + i;
+                
+
+                mail.To = TextAdresse.Text; //Récupere la valeur du textbox
+                                            //On définit le corps du message
+                mail.Body = TextMessage.Text;
+                mail.Send();
+                MessageBox.Show("Mail n" + i + "envoyé!");
+
+            }
+        }
+    }
+}
